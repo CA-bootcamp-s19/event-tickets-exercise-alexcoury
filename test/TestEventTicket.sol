1648ceff6410b6e6c7c45b95a968cc72e3f44ee5
Alex Coury <alexcoury@MacBook-Pro.localdomain>
2019-06-18 16:09:53 -0400

    Updated Tests

--- test/TestEventTicket.sol
+++ test/TestEventTicket.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.0;
 
-
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
 import "../contracts/EventTickets.sol";
 import "../contracts/EventTicketsV2.sol";
 
